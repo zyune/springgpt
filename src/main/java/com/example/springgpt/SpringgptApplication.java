991246db530d0044d6d06ed17d516c44fa3fa972
Zheng Yune <yunethecoder@gmail.com>
2023-04-18 12:54:15 -0500

    update a spring project

---
+++ src/main/java/com/example/springgpt/SpringgptApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgptApplication.class, args);
+	}
+
+}
