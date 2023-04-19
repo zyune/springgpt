2ad63b7a0bb9708586d9e963110dfdd0c3a942dc
Zheng Yune <yunethecoder@gmail.com>
2023-04-19 18:49:00 -0500

    update first version of gpt integrated, didn't restore the gpt asnwer to database yet

---
+++ src/main/java/com/example/springgpt/OpenAIController.java
@@ -0,0 +1,51 @@
+package com.example.springgpt;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/openai")
+public class OpenAIController {
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateText(@RequestParam(value = "prompt") String prompt) {
+        String url = "https://api.openai.com/v1/chat/completions";
+        String apiKey = "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";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(apiKey);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("model", "gpt-3.5-turbo");
+
+        JSONArray messages = new JSONArray();
+        JSONObject message = new JSONObject();
+        message.put("role", "user");
+        message.put("content", prompt);
+        messages.put(message);
+        requestBody.put("messages", messages);
+        System.out.println(requestBody);
+        // "{\n" +
+        // " \"model\":\"gpt-3.5-turbo\",\n" +
+        // " \"messages\":[\n" +
+        // " {\"role\": \"user\", \"content\": \"" + prompt + "\"}\n" +
+        // " ]\n" +
+        // "}";
+        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody.toString(), headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(
+                url,
+                requestEntity,
+                String.class);
+
+        return response;
+    }
+}
